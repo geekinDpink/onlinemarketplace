ec9bd519c3e40624612e46e28a5b4ed360d572d7
geekinthepink <rawisglenn@gmail.com>
2023-08-17 23:42:22 +0800

    Add verifytoken middleware to products api

--- server.js
+++ server.js
@@ -21,12 +21,9 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-// create middleware
+// create middleware to verify token, if able to verify i.e. no error, move on to implementation next middleware
 function verifyToken(req, res, next) {
-  console.log("req.header", req.headers);
-
   var token = req.headers["authorization"]; // retrieve authorization header’s content
-  // console.log(token);
 
   if (!token || !token.includes("Bearer")) {
     res.status(403); // 403 - Forbidden
@@ -34,7 +31,6 @@ function verifyToken(req, res, next) {
   } else {
     token = token.split("Bearer ")[1]; // split bearer from token and refer to token
     jwt.verify(token, secret, function (err, decoded) {
-      //verify token
       if (err) {
         res.status(403); // 403 - Forbidden
         return res.end({ auth: false, message: "Not authorized!" });
@@ -46,20 +42,6 @@ function verifyToken(req, res, next) {
     });
   }
 }
-
-// Get all users info
-// app.get("/users", (req, res) => {
-//   const { username, password } = req.body;
-//   const sql = "SELECT * FROM users_table";
-//   connection.query(sql, (err, users) => {
-//     if (err) {
-//       console.error("Error fetching users", err);
-//       res.status(500).json({ error: "Users information is not available " });
-//     } else {
-//       res.status(200).json(users);
-//     }
-//   });
-// });
 
 // Endpoint for login, take in username and password and generate jwt
 app.post("/login", (req, res) => {
@@ -85,7 +67,10 @@ app.post("/login", (req, res) => {
             expiresIn: 3600, // 1 hour
           }
         );
-        res.status(200).json(token); // 200 - OK
+        const theUser = user[0];
+        res
+          .status(200)
+          .json({ token: token, id: theUser.id, username: theUser.username }); // 200 - OK
       } else {
         res.status(404).json({ error: "Users information is not available " }); // 404 - Not Found
       }
@@ -93,21 +78,72 @@ app.post("/login", (req, res) => {
   });
 });
 
+// Add new user, hashed password and email to db
+app.post("/register", (req, res) => {
+  const { username, password, email } = req.body;
+
+  // hash the password
+  bcrypt
+    .hash(password, 5)
+    .then((hashedPassword) => {
+      const sql =
+        "INSERT INTO users_table (username, password, email) VALUES (?, ?, ?)";
+      connection.query(
+        sql,
+        [username, hashedPassword, email],
+        (err, result) => {
+          if (err) {
+            console.error(`Error registering ${username}`, err);
+            res.status(500).json({ error: "Unable to register user" }); // 500 - Internal Server Error
+          } else {
+            // 201 - Created
+            res.status(201).json({
+              id: result.insertId,
+              message: `${username} added successfully`,
+            });
+          }
+        }
+      );
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      res.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
+});
+
 // Get all products info
-app.get("/products", verifyToken, (req, res) => {
+app.get("/products", (req, res) => {
   const sql = "SELECT * FROM products_table";
-  connection.query(sql, (err, users) => {
+  connection.query(sql, (err, products) => {
     if (err) {
       console.error(`Error fetching products`, err);
       res.status(500).json({ error: "Products information is not available" }); // 500 - Internal Server Error
     } else {
-      res.status(200).json(users); // 200 - OK
+      res.status(200).json(products); // 200 - OK
+    }
+  });
+});
+
+// Get products info of a specific seller
+app.get("/products/:id", verifyToken, (req, res) => {
+  const sellerId = req.params.id;
+  const sql = "SELECT * FROM products_table WHERE SELLER_ID LIKE ?";
+
+  connection.query(sql, [sellerId], (err, products) => {
+    if (err) {
+      console.error(`Error fetching products`, err);
+      res.status(500).json({ error: "Products information is not available" }); // 500 - Internal Server Error
+    } else {
+      res.status(200).json(products); // 200 - OK
     }
   });
 });
 
 // Add new product
-app.post("/products", (req, res) => {
+app.post("/products", verifyToken, (req, res) => {
   const { seller_id, seller_name, item_name, item_desc, price } = req.body;
   const sql =
     "INSERT INTO products_table (seller_id, seller_name, item_name, item_desc, price) VALUES (?, ?, ?, ?, ?)";
